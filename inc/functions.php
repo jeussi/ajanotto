f3c05329b6ac52baf93af2404112b7c357bc47c9
jeussi <jeussi.lindholm@gmail.com>
2024-11-24 17:52:49 +0200

    Made role specific function

--- inc/functions.php
+++ inc/functions.php
@@ -9,3 +9,11 @@ function tarkistaKirjautuminen()
     return false;
   }
 }
+
+function tarkistaRooli($vaadittuRooli) {
+  if (isset($_SESSION['kirjautunut']) && $_SESSION['kirjautunut'] === true) {
+      return isset($_SESSION['rooli']) && $_SESSION['rooli'] === $vaadittuRooli;
+  }
+  return false;
+}
+
