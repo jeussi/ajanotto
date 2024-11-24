4a2ce03844798791fb166aa57b7e61570d1746c4
jeussi <jeussi.lindholm@gmail.com>
2024-11-24 13:52:07 +0200

    test commit 2

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include_once 'inc/header.php';
 ?>
 
 <div class="container">
-  <h1 class="mt-5">Ajanottojärjestelmä</h1>
+  <h1 class="mt-5">Ajanottojärjestelmä!</h1>
   <p class="lead">Tervetuloa ajanottojärjestelmään.</p>
 </div>
 
