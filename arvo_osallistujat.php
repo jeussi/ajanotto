aa9d4fe1a7498fda94306dfc5eeb7aba6010efaa
jeussi <jeussi.lindholm@gmail.com>
2025-01-21 15:52:05 +0200

    added some basic html, needs php funcionality

--- arvo_osallistujat.php
+++ arvo_osallistujat.php
@@ -1,9 +1,19 @@
-<?php 
+<?php
 include_once 'inc/header.php';
 include_once 'inc/functions.php';
 include_once 'inc/database.php';
 ?>
 
 <div class="container">
-  <h1>Tähän sivulle tulee osallistujalistojen arpominen</h1>
+  <form method="post" action="arvo_osallistujat.php">
+    <label for="vaihe">Valitse vaihe:</label>
+    <select name="vaihe" id="vaihe" required>
+      <option value="alkuera">Alkuerä</option>
+      <option value="valiera">Välierä</option>
+    </select>
+
+    <label for="joukkueet">Valitse joukkueet:</label>
+
+    <button type="submit">Arvo listat</button>
+  </form>
 </div>
