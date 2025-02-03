34d7c0d22140dc9516b7784ec721149e86d3345b
jeussi <jeussi.lindholm@gmail.com>
2025-02-03 16:12:34 +0200

    changed id's to joukkueid and updated where the data is taken from sql

--- tulospalvelu.php
+++ tulospalvelu.php
@@ -2,23 +2,23 @@
 include_once 'inc/header.php';
 include_once 'inc/database.php';
 
-$vaihe = $_GET['vaihe'] ?? null;
+$era = $_GET['era'] ?? null;
 
-$sql = "SELECT joukkueet.id AS joukkue_id, joukkueet.nimi AS joukkue_nimi, erat.vaihe,
-        erat.tehtava1_aika, erat.tehtava2_aika, erat.tehtava3_aika, erat.kokonaisaika
-        FROM erat
-        INNER JOIN joukkueet ON erat.joukkue_id = joukkueet.id
+$sql = "SELECT joukkueet.joukkueid, joukkueet.nimi AS joukkue_nimi, tulostaulu.era,
+        tulostaulu.tehtava1aika, tulostaulu.tehtava2aika, tulostaulu.tehtava3aika, tulostaulu.kokonaisaika
+        FROM tulostaulu
+        INNER JOIN joukkueet ON tulostaulu.joukkueid = joukkueet.joukkueid
 ";
 
-if ($vaihe) {
-  $sql .= " WHERE vaihe = :vaihe";
+if ($era) {
+  $sql .= " WHERE era = :era";
 }
-$sql .= " ORDER BY erat.kokonaisaika ASC";
+$sql .= " ORDER BY tulostaulu.kokonaisaika ASC";
 
 $stmt = $pdo->prepare($sql);
 
-if ($vaihe) {
-  $stmt->execute([':vaihe' => $vaihe]);
+if ($era) {
+  $stmt->execute([':era' => $era]);
 } else {
   $stmt->execute();
 }
@@ -43,10 +43,10 @@ $tulokset = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
       <div class="mb-3 mt-2">
         <a href="?"><button class="btn btn-success">Kaikki erät</button></a>
-        <a href="?vaihe=alkuera"><button class="btn btn-success">Alkuerät</button></a>
-        <a href="?vaihe=kerailyera"><button class="btn btn-success">Keräilyerät</button></a>
-        <a href="?vaihe=valiera"><button class="btn btn-success">Välierät</button></a>
-        <a href="?vaihe=finaali"><button class="btn btn-success">Finaali</button></a>
+        <a href="?era=alkuera"><button class="btn btn-success">Alkuerät</button></a>
+        <a href="?era=kerailyera"><button class="btn btn-success">Keräilyerät</button></a>
+        <a href="?era=valiera"><button class="btn btn-success">Välierät</button></a>
+        <a href="?era=finaali"><button class="btn btn-success">Finaali</button></a>
       </div>
 
       <?php if (count($tulokset) > 0): ?>
@@ -65,12 +65,12 @@ $tulokset = $stmt->fetchAll(PDO::FETCH_ASSOC);
           <tbody>
             <?php foreach ($tulokset as $tulos): ?>
               <tr>
-                <td><?= ($tulos['joukkue_id']) ?></td>
+                <td><?= ($tulos['joukkueid']) ?></td>
                 <td><?= ($tulos['joukkue_nimi']) ?></td>
-                <td><?= ($tulos['vaihe']) ?></td>
-                <td><?= ($tulos['tehtava1_aika']) ?></td>
-                <td><?= ($tulos['tehtava2_aika']) ?></td>
-                <td><?= ($tulos['tehtava3_aika']) ?></td>
+                <td><?= ($tulos['era']) ?></td>
+                <td><?= ($tulos['tehtava1aika']) ?></td>
+                <td><?= ($tulos['tehtava2aika']) ?></td>
+                <td><?= ($tulos['tehtava3aika']) ?></td>
                 <td><?= ($tulos['kokonaisaika']) ?></td>
               </tr>
             <?php endforeach; ?>
